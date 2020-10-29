2a7a8dbc63858be596bc829e6e82be47aab672cc
navroz-lamba <lamba-navroz@lambdastudents.com>
2020-10-29 14:18:23 -0400

    solution with list comp

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -24,3 +24,17 @@ if there is a mathematical approach that you can take.*
 def to_lower_case(string):
     # Your code here
 
+#     new_string = ""
+
+#     for i in string:
+#         if ord(i) > 65 or ord(i) < 90:
+#            new_string += chr(ord(i) + 32)
+#         else: 
+#             new_string += i
+
+#     return new_string
+
+# print(to_lower_case("LLAMA"))
+
+    return ''.join([chr(ord(c) + 32) if ord(c) > 64 and \
+    ord(c) < 91 else c for c in string])
